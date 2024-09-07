c4db54e675af1f3f96d3d4606faccde1f898889a
venkatesh966 <venkateshmorpoju@gmail.com>
2024-09-06 23:47:17 -0700

    added eslint disable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { Component } from "react";
 import ReactGA from "react-ga";
 import $ from "jquery";
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { Component } from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
